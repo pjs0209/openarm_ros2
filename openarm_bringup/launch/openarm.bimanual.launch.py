efcf11cb4d307c0be0a66c280eac4cda086746da
jun <pjs0209@konkuk.ac.kr>
2026-02-25 19:30:38 +0900

    My modifications on openarm_ros2

--- openarm_bringup/launch/openarm.bimanual.launch.py
+++ openarm_bringup/launch/openarm.bimanual.launch.py
@@ -159,7 +159,7 @@ def generate_launch_description():
         ),
         DeclareLaunchArgument(
             "robot_controller",
-            default_value="joint_trajectory_controller",
+            default_value="forward_position_controller",
             choices=["forward_position_controller",
                      "joint_trajectory_controller"],
             description="Robot controller to start.",
